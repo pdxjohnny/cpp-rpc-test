7f6a6a4ad599b7d32f47f3fce6c1ca8687f585d6
John Andersen <johnandersenpdx@gmail.com>
2016-03-30 22:43:24 +0000

    Forking and having parent tell server to stop
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,59 @@
 #include "rpc-test.h"
 
 int main (int argc, char ** argv, char ** env) {
+    int err;
+
     printf("Hello rpc lib %d\n", rpc_test());
 
+    // Create a pipe so we can tell the server when to stop running
+    int pipe_fd[2];
+    err = pipe(pipe_fd);
+    if (err == -1) {
+        printf("Error creating pipe %d\n", errno);
+    }
+
+    // Configure the server
+    char addr[] = "127.0.0.1";
     struct rpc_handler handlers[] = {
         NULL
     };
-    rpc_start_server(handlers);
+    struct rpc_server_config config = {
+        .addr = addr,
+        .port = 0,
+        .handlers = handlers,
+        .comm = pipe_fd[0],
+    };
+
+    // Store the server exit status in this variable
+    int server_exit;
+
+    // Fork so that the server is running in the background
+    switch (fork()) {
+    case -1:
+        // Error
+        printf("Error while forking %d\n", errno);
+        return 1;
+
+    case 0:
+        // Clild
+        close(pipe_fd[1]);
+        printf("About to start server\n");
+        server_exit = rpc_start_server(&config);
+        printf("Server exited with status %d\n", server_exit);
+
+    default:
+        // Parent
+        // We dont read so just close the read end
+        close(pipe_fd[0]);
+        // Make a request (for now just sleep a bit)
+        // sleep(1);
+        // Send some data trough, the server will stop once we do
+        char stop_msg[] = "stop";
+        write(pipe_fd[1], stop_msg, strlen(stop_msg));
+        close(pipe_fd[1]);
+        printf("Client exited with status 0\n");
+    }
+
+    return 0;
 }
 
--- src/rpc-test.h
+++ src/rpc-test.h
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
 
 #include <librpc.h>
 
